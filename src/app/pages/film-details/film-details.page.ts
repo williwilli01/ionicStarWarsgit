d9a4ba382753f74677251b2c194d4f0bcad1f95f
Svenja Schramm <svenja@Svenjas-iMac.fritz.box>
2019-11-01 11:43:09 +0100

    191101

--- src/app/pages/film-details/film-details.page.ts
+++ src/app/pages/film-details/film-details.page.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-film-details',
   templateUrl: './film-details.page.html',
   styleUrls: ['./film-details.page.scss'],
 })
+
 export class FilmDetailsPage implements OnInit {
 
-  constructor() { }
-
+    filmId = null;
+ 
+  constructor(private activatedRoute: ActivatedRoute) { }
+ 
   ngOnInit() {
+    this.filmId = this.activatedRoute.snapshot.paramMap.get('id');
   }
 
 }
